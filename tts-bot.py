af2bf5df04c6d147e25f84273e9a4b99c349713a
fractiwi <64674935+kiwijuice56@users.noreply.github.com>
2022-08-04 16:38:02 -0500

    Quality of life improvements
    Successive messages by one person won't trigger the bot to repeat their name. Emotes and links are ignored. As of now, the bot still needs to detect user pings and convert instances to nicknames.

--- tts-bot.py
+++ tts-bot.py
@@ -1,12 +1,12 @@
 import os
 import base64
 import requests as requests
+import time
+from collections import deque
 
 import discord
 from discord.ext import tasks, commands
 
-import time
-from collections import deque
 
 # Initialize the bot
 id_dir = os.path.dirname(__file__)
@@ -15,7 +15,7 @@ CHANNEL_ID = int(open(os.path.join(id_dir, "channel.txt"), "r").readline())
 TOKEN = open(os.path.join(id_dir, "token.txt"), "r").readline()
 VALID_VOICES = set([voice.strip() for voice in open(os.path.join(id_dir, "valid_voices.txt"), "r").readlines()])
 
-INACTIVITY_TIMEOUT = 30
+INACTIVITY_TIMEOUT = 120
 voice_text_channel = None
 
 bot = commands.Bot(command_prefix=".", intents=discord.Intents().all())
@@ -25,11 +25,12 @@ user_profiles = {}
 message_queue = deque()
 is_playing = False
 last_message_time = time.time()
+last_talker = None
 
 
 # Classes to store user and request data
 class UserProfile:
-    def __init__(self, user, is_talking=True, voice="en_us_002", say_name=True):
+    def __init__(self, user, is_talking=True, voice="en_us_002", say_name=False):
         self.user = user
         self.is_talking = is_talking
         self.say_name = say_name
@@ -42,16 +43,22 @@ class TTSMessage:
         self.message = message
 
     def play(self):
+        global last_talker
+
         profile = user_profiles[self.user]
+
+        content = self.message
+        if profile.say_name and not last_talker == self.user:
+            content = f"{self.user.display_name} said " + content
 
         # Create the mp3 speech file
         audio_filename = f"voice_messages/out{int(time.time())}.mp3"
-        content = (f"{self.user.display_name} said " if profile.say_name else "") + self.message.content
         create_tts_mp3(content, profile.voice, audio_filename)
 
         # Get the voice client and play the mp3
         voice_client = discord.utils.get(bot.voice_clients, guild=self.user.guild)
         voice_client.play(discord.FFmpegPCMAudio(source=audio_filename), after=lambda x: advance_message_queue())
+        last_talker = self.user
 
 
 @bot.event
@@ -70,19 +77,30 @@ async def on_ready():
 async def on_message(message):
     global is_playing
 
+    # Process commands
     if message.content.startswith(bot.command_prefix):
         await bot.process_commands(message)
         return
-    if not message.channel == voice_text_channel:
-        return
-    user = message.author
 
+    user = message.author
+    
     if user not in user_profiles:
         return
+    
     profile = user_profiles[user]
 
     # Check that the user is in a voice channel and has enabled talking
-    if not (user.voice and profile.is_talking):
+    if not (user.voice and message.channel == voice_text_channel and profile.is_talking):
+        return
+
+    words = []
+    for word in message.content.split():
+        if word.startswith("http") or word.startswith(":"):
+            continue
+        words.append(word)
+    stripped_message = " ".join(words)
+
+    if len(stripped_message) == 0:
         return
 
     # Check that the bot is in the correct voice channel
@@ -90,7 +108,7 @@ async def on_message(message):
     if not (voice_client and voice_client.is_connected()):
         await user.voice.channel.connect()
 
-    message_queue.append(TTSMessage(user, message))
+    message_queue.append(TTSMessage(user, stripped_message))
 
     if not is_playing:
         is_playing = True
@@ -118,24 +136,23 @@ async def config(ctx, *args):
     user = ctx.message.author
     if user not in user_profiles:
         user_profiles[user] = UserProfile(user, is_talking=False)
-    match args[0]:
-        case "voice":
-            if args[1] in VALID_VOICES:
-                user_profiles[user].voice = args[1]
-                await ctx.message.channel.send(f":pencil: *{user}*, your voice has been updated to `{args[1]}`")
-            else:
-                await ctx.message.channel.send(f":x: *{user}*, the voice you selected does not exist. "
-                                               f"Search for available voices with `voicelist`")
-        case "name":
-            if args[1].lower() in ["true", "t", "yes", "y"]:
-                user_profiles[user].say_name = True
-            elif args[1].lower() in ["false", "f", "no", "n"]:
-                user_profiles[user].say_name = False
-            else:
-                await ctx.message.channel.send(f":x: *{user}*, your choice could not be parsed as True or False")
-                return
-            await ctx.message.channel.send(f":pencil: *{user}*, "
-                                           f"your name status has been updated to `{user_profiles[user].say_name}`")
+    if args[0] == "voice":
+        if args[1] in VALID_VOICES:
+            user_profiles[user].voice = args[1]
+            await ctx.message.channel.send(f":pencil: *{user}*, your voice has been updated to `{args[1]}`")
+        else:
+            await ctx.message.channel.send(f":x: *{user}*, the voice you selected does not exist. "
+                                           f"Search for available voices with `voicelist`")
+    elif args[0] == "name":
+        if args[1].lower() in ["true", "t", "yes", "y"]:
+            user_profiles[user].say_name = True
+        elif args[1].lower() in ["false", "f", "no", "n"]:
+            user_profiles[user].say_name = False
+        else:
+            await ctx.message.channel.send(f":x: *{user}*, your choice could not be parsed as True or False")
+            return
+        await ctx.message.channel.send(f":pencil: *{user}*, "
+                                       f"your name status has been updated to `{user_profiles[user].say_name}`")
 
 
 @bot.command()
@@ -158,7 +175,7 @@ async def info(ctx):
 async def voicelist(ctx):
     embed = discord.Embed(
         title="Voice List",
-        description=", ".join([f"`{voice}`" for voice in VALID_VOICES])
+        description=", ".join(sorted([f"`{voice}`" for voice in VALID_VOICES]))
     )
     await ctx.message.channel.send(embed=embed)
 
@@ -182,7 +199,7 @@ async def myprofile(ctx):
 @tasks.loop(seconds=2.0)
 async def activity_check():
     voice = discord.utils.get(bot.voice_clients)
-    if (not is_playing) and voice and voice.is_connected() and time.time() - last_message_time > INACTIVITY_TIMEOUT:
+    if voice and voice.is_connected() and time.time() - last_message_time > INACTIVITY_TIMEOUT and not is_playing:
         await voice.disconnect()
         await voice_text_channel.send(f":pause_button: Left voice channel after {INACTIVITY_TIMEOUT} "
                                       f"seconds of inactivity. Talk to prompt the bot to rejoin")
@@ -196,20 +213,17 @@ def advance_message_queue():
             os.remove(os.path.join(id_dir, "voice_messages/" + file))
 
         is_playing = False
-        return
-    voice_request = message_queue.popleft()
-    last_message_time = time.time()
-    voice_request.play()
+    else:
+        voice_request = message_queue.popleft()
+        last_message_time = time.time()
+        voice_request.play()
 
 
-# Modified from a script to download tts mp3 files from TikTok API:
+# Modified from a script to download TTS mp3 files from TikTok API:
 # https://github.com/oscie57/tiktok-voice
 def create_tts_mp3(text, voice, filename):
-    # Use TikTok API to request a TTS audio file
     url = f"https://api16-normal-useast5.us.tiktokv.com/media/api/text/speech/invoke/?" \
-          f"text_speaker={voice}&" \
-          f"req_text={text}" \
-          f"&speaker_map_type=0"
+          f"text_speaker={voice}&req_text={text}&speaker_map_type=0"
 
     r = requests.post(url)
 
